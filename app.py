d9b69617ed315ef3f6239d9b8bd73e26a6454774
Jojin <jojinjoseph@gmail.com>
2026-02-11 13:14:29 +0400

    app file changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python Web App! Python web app → Docker → Git → ACR → AKS → Jenkins automation."
+    return "Hello from Python Web App! Jojin has done it → Docker → Git → ACR → AKS → Jenkins automation."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
